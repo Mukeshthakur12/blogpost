7f0cb4f11415490338d0c2a2888b58503fcc5b12
mukesh <mukeshs34114@gmail.com>
2026-02-16 17:28:06 +0530

    first commit

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: process.env.SITE_URL || 'https://example.com',
+    siteUrl: process.env.SITE_URL || 'https://tech-blog-demo.vercel.app',
     generateRobotsTxt: true,
     // optional
     robotsTxtOptions: {
